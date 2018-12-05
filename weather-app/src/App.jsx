dea44d04cd3ad13d422bed7563312db3c8c8cca6
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-12-04 22:47:02 -0600

    stateless components

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -25,15 +25,26 @@ class App extends React.Component {
     );
 
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter a city and country"
+      });
+    }
   };
   render() {
     return (
